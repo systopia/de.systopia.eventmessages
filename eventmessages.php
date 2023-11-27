eaf8d7bad1e6982a723827a4b4958dd2a444209c
Johannes <margraf@systopia.de>
2023-11-27 15:25:19 +0100

    fix issue #43 with civix upgrade

--- eventmessages.php
+++ eventmessages.php
@@ -52,26 +52,6 @@ function eventmessages_civicrm_install()
 }
 
 /**
- * Implements hook_civicrm_postInstall().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_postInstall
- */
-function eventmessages_civicrm_postInstall()
-{
-    _eventmessages_civix_civicrm_postInstall();
-}
-
-/**
- * Implements hook_civicrm_uninstall().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_uninstall
- */
-function eventmessages_civicrm_uninstall()
-{
-    _eventmessages_civix_civicrm_uninstall();
-}
-
-/**
  * Implements hook_civicrm_enable().
  *
  * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_enable
@@ -79,38 +59,6 @@ function eventmessages_civicrm_uninstall()
 function eventmessages_civicrm_enable()
 {
     _eventmessages_civix_civicrm_enable();
-}
-
-/**
- * Implements hook_civicrm_disable().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_disable
- */
-function eventmessages_civicrm_disable()
-{
-    _eventmessages_civix_civicrm_disable();
-}
-
-/**
- * Implements hook_civicrm_upgrade().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_upgrade
- */
-function eventmessages_civicrm_upgrade($op, CRM_Queue_Queue $queue = null)
-{
-    return _eventmessages_civix_civicrm_upgrade($op, $queue);
-}
-
-/**
- * Implements hook_civicrm_entityTypes().
- *
- * Declare entity types provided by this module.
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_entityTypes
- */
-function eventmessages_civicrm_entityTypes(&$entityTypes)
-{
-    _eventmessages_civix_civicrm_entityTypes($entityTypes);
 }
 
 /**
