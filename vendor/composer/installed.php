db5f6eced53a98b362e1357019008cb96ca45f60
Jens Schuppe <schuppe@systopia.de>
2026-01-30 14:14:05 +0100

    Set version to 1.6.0-beta1

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'systopia/de.systopia.eventmessages',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '2d1a58adc429fbb6c373a275bcd00653c584ad59',
+        'type' => 'civicrm-ext',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'systopia/de.systopia.eventmessages' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '2d1a58adc429fbb6c373a275bcd00653c584ad59',
+            'type' => 'civicrm-ext',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
